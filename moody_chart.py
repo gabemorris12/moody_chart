32543b84db228cc4ca9413d133ac08f8dea7285e
Gabe Morris <gabemorris1231@gmail.com>
2021-12-08 19:54:39 -0600

    almost done

--- moody_chart.py
+++ moody_chart.py
@@ -1,21 +1,65 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-friction_factors = [0.008, 0.009, 0.01, 0.015, 0.02, 0.025, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1]
-major_friction = np.arange(0.01, friction_factors[-1] + 0.01, 0.01)
-minor_friction = sorted(list(set(friction_factors).difference(major_friction)))
+# Choose which relative roughness lines to plot and the major and minor ticks for the friction factor axis
+relative_roughness = [0.00001, 0.00005, 0.0001, 0.0002, 0.0004, 0.0006, 0.0008, 0.001, 0.002, 0.004, 0.006, 0.008, 0.01,
+                      0.015, 0.02, 0.03, 0.04, 0.05]
+major_ticks = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1]
+minor_ticks = [0.008, 0.009, 0.015]
 
-fig, ax1 = plt.subplots()  # You can do more with figure and axis object
+fig, ax = plt.subplots()  # You can do more with figure and axis object
 
 # Plotting the laminar line
-Re_lam = np.linspace(64/max(friction_factors), 2300, 1000)
-ax1.set_xscale('log')
-ax1.set_yscale('log')
-ax1.plot(Re_lam, 64/Re_lam)
+Re_lam = np.linspace(64/0.1, 2300, 1000)
+Re_transition = np.linspace(2300, 10_000, 1000)
+ax.set_xscale('log')
+ax.set_yscale('log')
+ax.plot(Re_lam, 64/Re_lam, color='black')
+ax.plot(Re_transition, 64/Re_transition, color='black', ls='--')
+
+# Plotting the relative roughness lines
+Re_turbulent = np.linspace(10_000, 1e8, 100_000)
+for r in relative_roughness:
+    f = (1/(-1.8*np.log10((r/3.7)**1.11 + 6.9/Re_turbulent)))**2  # Haaland Equation
+    f_trans = (1/(-1.8*np.log10((r/3.7)**1.11 + 6.9/Re_transition)))**2
+    ax.plot(Re_turbulent, f, color='maroon')
+    ax.plot(Re_transition, f_trans, color='maroon', ls='--')
+    if r != 0.001:  # This is just to fix the issue with the 0.001 being too close to 0.0008
+        ax.annotate(f'{r:.5f}'.rstrip('0'), (1.2e8, np.min(f)), va='center',
+                    bbox=dict(facecolor='white', pad=0, edgecolor='white'))
+    else:
+        ax.annotate(f'{r:.5f}'.rstrip('0'), (1.2e8, np.min(f)),
+                    bbox=dict(facecolor='white', pad=0, edgecolor='white'))
+
+# Make the laminar, transition, and turbulent intervals
+# For more details on annotate: https://matplotlib.org/1.5.3/users/annotations_guide.html
+arrow_style = dict(arrowstyle='<|-|>', connectionstyle='arc3', color='deepskyblue', lw=1.5,
+                   shrinkB=0, shrinkA=0)  # ShrinkA and shrinkB are set to connect, meaning no space.
+bbox_parameter = dict(facecolor='white', edgecolor='white', pad=0)
+vertical = 0.085
+ax.annotate('', xy=(64/0.1, vertical), xytext=(2300, vertical), arrowprops=arrow_style)
+ax.annotate('', xy=(2300, vertical), xytext=(10_000, vertical), arrowprops=arrow_style)
+ax.annotate('', xy=(10_000, vertical), xytext=(1e8, vertical), arrowprops=arrow_style)
+ax.annotate('Laminar', xy=(np.sqrt(64/0.1*2300), vertical + 0.002), va='bottom', ha='center', size='small',
+            bbox=bbox_parameter)
+ax.annotate('Transition', xy=(np.sqrt(2300*10_000), vertical + 0.002), va='bottom', ha='center', size='small',
+            bbox=bbox_parameter)
+ax.annotate('Turbulent', xy=(np.sqrt(10_000*1e8), vertical + 0.002), va='bottom', ha='center', size='small',
+            bbox=bbox_parameter)
 
 # Grid and Axis Labels
-ax1.set_yticks(ticks=major_friction, labels=[f'{i:.2f}' for i in major_friction])
-ax1.set_yticks(ticks=minor_friction, labels=[str(i) for i in minor_friction], minor=True)
-ax1.grid(which='minor', ls='--')
-ax1.grid(which='major')
+# Do not need to call ax.minorticks_on() if the following gets called:
+ax.set_yticks(ticks=major_ticks, labels=[str(i) for i in major_ticks])
+ax.set_yticks(ticks=minor_ticks, labels=[str(i) for i in minor_ticks], minor=True)
+ax.grid(which='minor', ls='--')
+ax.grid(which='major')
+ax.set_xlim(np.min(Re_lam), 4e8)
+ax.legend([ax.lines[0], ax.lines[2]],
+          [r'Laminar Flow Line ($f=64/Re$)', r'Relative Roughness Lines ($\epsilon/D$)'], ncol=2)
+ax.set_ylabel(r'Friction Factor ($f=-\frac{\partial P}{\partial x}\frac{D}{\rho {U_m}^2/2}$)')
+ax.set_xlabel(r"Reynold's Number ($Re=\frac{U_mD}{\nu}$)")
+ax.set_title('Moody Chart')
+
+fig.set_size_inches(8.5, 11)
+# fig.savefig('scratch.pdf')
 plt.show()
