ebefd7725e614c6bb67e5034b4227bdbc0928b5e
Gabe Morris <gabemorris1231@gmail.com>
2022-05-16 19:11:38 -0500

    updated according to our book

--- moody_chart.py
+++ moody_chart.py
@@ -16,20 +16,20 @@ minor_ticks = [0.008, 0.009, 0.0125, 0.015, 0.025]
 fig, ax = plt.subplots()  # You can do more with figure and axis object
 
 # Plotting the laminar line
-Re_lam = np.linspace(64/0.1, 2300, 1000)
-Re_transition = np.linspace(2300, 10_000, 1000)
+Re_lam = np.linspace(64/0.1, 2000, 1000)
+Re_critical = np.linspace(2000, 4500, 1000)
 ax.set_xscale('log')
 ax.set_yscale('log')
 ax.plot(Re_lam, 64/Re_lam, color=laminar_line_color)
-ax.plot(Re_transition, 64/Re_transition, color=laminar_line_color, ls='--')
+ax.plot(Re_critical, 64/Re_critical, color=laminar_line_color, ls='--')
 
 # Plotting the relative roughness lines
-Re_turbulent = np.linspace(10_000, 1e8, 100_000)
+Re_turbulent = np.linspace(4500, 1e8, 100_000)
 for r in relative_roughness:
-    f = (1/(-1.8*np.log10((r/3.7)**1.11 + 6.9/Re_turbulent)))**2  # Haaland Equation
-    f_trans = (1/(-1.8*np.log10((r/3.7)**1.11 + 6.9/Re_transition)))**2
+    f = 0.3086/((np.log10(6.9/Re_turbulent + (r/3.7)**1.11))**2)  # Haaland Equation (p. 10)
+    f_trans = (1/(-1.8*np.log10((r/3.7)**1.11 + 6.9/Re_critical)))**2
     ax.plot(Re_turbulent, f, color=relative_roughness_color)
-    ax.plot(Re_transition, f_trans, color=relative_roughness_color, ls='--')
+    ax.plot(Re_critical, f_trans, color=relative_roughness_color, ls='--')
     if r != 0.001:  # This is just to fix the issue with the 0.001 being too close to 0.0008
         ax.annotate(f'{r:.5f}'.rstrip('0'), (1.2e8, np.min(f)), va='center',
                     bbox=dict(facecolor='white', pad=0, edgecolor='white'))
@@ -37,20 +37,20 @@ for r in relative_roughness:
         ax.annotate(f'{r:.5f}'.rstrip('0'), (1.2e8, np.min(f)),
                     bbox=dict(facecolor='white', pad=0, edgecolor='white'))
 
-# Make the laminar, transition, and turbulent intervals
+# Make the laminar, critical, and turbulent intervals
 # For more details on annotate: https://matplotlib.org/1.5.3/users/annotations_guide.html
 arrow_style = dict(arrowstyle='<|-|>', connectionstyle='arc3', color=interval_arrows_color, lw=1.5,
                    shrinkB=0, shrinkA=0)  # ShrinkA and shrinkB are set to connect, meaning no space.
 bbox_parameter = dict(facecolor='white', edgecolor='white', pad=0)
 vertical = 0.088
-ax.annotate('', xy=(64/0.1, vertical), xytext=(2300, vertical), arrowprops=arrow_style)
-ax.annotate('', xy=(2300, vertical), xytext=(10_000, vertical), arrowprops=arrow_style)
-ax.annotate('', xy=(10_000, vertical), xytext=(1e8, vertical), arrowprops=arrow_style)
-ax.annotate('Laminar', xy=(np.sqrt(64/0.1*2300), vertical + 0.002), va='bottom', ha='center', size='small',
+ax.annotate('', xy=(64/0.1, vertical), xytext=(2000, vertical), arrowprops=arrow_style)
+ax.annotate('', xy=(2000, vertical), xytext=(4500, vertical), arrowprops=arrow_style)
+ax.annotate('', xy=(4500, vertical), xytext=(1e8, vertical), arrowprops=arrow_style)
+ax.annotate('Laminar', xy=(np.sqrt(64/0.1*2000), vertical + 0.002), va='bottom', ha='center', size='small',
             bbox=bbox_parameter)
-ax.annotate('Transition', xy=(np.sqrt(2300*10_000), vertical + 0.002), va='bottom', ha='center', size='small',
+ax.annotate('Critical', xy=(np.sqrt(2000*4500), vertical + 0.002), va='bottom', ha='center', size='small',
             bbox=bbox_parameter)
-ax.annotate('Turbulent', xy=(np.sqrt(10_000*1e8), vertical + 0.002), va='bottom', ha='center', size='small',
+ax.annotate('Turbulent', xy=(np.sqrt(4500*1e8), vertical + 0.002), va='bottom', ha='center', size='small',
             bbox=bbox_parameter)
 
 # Grid and Axis Labels
@@ -61,12 +61,11 @@ ax.grid(which='minor', ls='--')
 ax.grid(which='major')
 ax.set_xlim(np.min(Re_lam), 4e8)
 ax.set_ylim(0.007, 0.12)
-ax.legend([ax.lines[0], ax.lines[2]],
-          [r'Laminar Flow Line ($f=64/Re$)', r'Relative Roughness Lines ($\epsilon/D$)'], ncol=2)
+fig.legend([ax.lines[0], ax.lines[2]],
+           [r'Laminar Flow Line ($f=64/Re$)', r'Relative Roughness Lines ($\epsilon/D$)'], ncol=2, loc='upper center')
 ax.set_ylabel(r'Friction Factor ($f=-\frac{\partial P}{\partial x}\frac{D}{\rho {U_m}^2/2}$)')
 ax.set_xlabel(r"Reynold's Number ($Re=\frac{U_mD}{\nu}$)")
 ax.set_title('Moody Chart')
-
 
 if save_pdf:
     fig.set_size_inches(8.5, 11)
